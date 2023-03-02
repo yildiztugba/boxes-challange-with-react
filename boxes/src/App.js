aa281c7a37a66a92b820f49ea4c584c5c2d74648
yildiztugba <okra.yildiz@gmail.com>
2023-03-02 10:21:09 +0100

    render squares with map

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react"
+import boxes from './boxes'
+import Box from './component/Box'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+const [squares, setsquare] = React.useState(boxes)
+
+ const styles = {
+        backgroundColor: "black"
+    }
+
+   
+ const squareElements = squares.map(square => (
+       <div 
+            style={styles} 
+            className="box"
+           
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </div>
+    ))
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
 }
 
 export default App;
